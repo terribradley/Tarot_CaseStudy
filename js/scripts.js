d44f5a94d1bb2e35e3ce772381a7505226bc3f38
Terri Bradley <terribradley3@gmail.com>
2016-09-30 16:13:19 -0700

    got scroll plugin to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,38 @@
 $("document").ready(function() {
-				$(".corner").click(function(){
+	$(".intro-link").click(function(){
 
-					$("html, body").animate({
-						scrollTop: $("#overview").offset().top
-					}, 2000);
-				 });
+		$("html, body").animate({
+			scrollTop: $("#overview").offset().top
+		}, 2000);
+	});
+	$(".context-link").click(function(){
 
-         $(".headline").scrollPath({
-             drawPath: true,
-             wrapAround: true,
-             scrollBar: false
-         });
+		$("html, body").animate({
+			scrollTop: $("#context").offset().top
+		}, 2000);
+	});
+	$(".process-link").click(function(){
+
+		$("html, body").animate({
+			scrollTop: $("#process").offset().top
+		}, 2000);
+	});
+	$(".solution-link").click(function(){
+
+		$("html, body").animate({
+			scrollTop: $("#solution").offset().top
+		}, 2000);
+	});
+	$(".result-link").click(function(){
+
+		$("html, body").animate({
+			scrollTop: $("#result").offset().top
+		}, 2000);
+	});
+
+	// $(".headline").scrollPath({
+	// 	drawPath: true,
+	// 	wrapAround: true,
+	// 	scrollBar: false
+	// });
+});
